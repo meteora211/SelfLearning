ca070d2e48dffd9fb7915f8b0c70da7b2e3c47b8
Meteora <czhaor@gmail.com>
2018-08-17 12:53:06 +0800

    add solution for problem 007

---
+++ Leetcode/Math/007.ReverseInteger/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "solution.h"
+using namespace std;
+
+int main()
+{
+	Solution s;
+	int input = -123;
+	int output = s.reverse(input);
+	cout << "output is : " << output << endl;
+	return 0;
+}
