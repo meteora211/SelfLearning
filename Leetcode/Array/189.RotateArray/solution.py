4759dbe97e82819ca763fd18a6aef038cad49eb5
Meteora <czhaor@gmail.com>
2018-10-11 20:13:03 +0800

    add python 茴字的写法

---
+++ Leetcode/Array/189.RotateArray/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        k %= length
+        nums[:k], nums[k:] = nums[-k:], nums[:length - k]
