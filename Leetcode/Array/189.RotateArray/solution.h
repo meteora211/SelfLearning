0d82127d12af9b04acdde64dd0fd70758424a3c9
Meteora <czhaor@gmail.com>
2018-10-11 19:34:48 +0800

    茴字的三种写法

--- Leetcode/Array/189.RotateArray/solution.h
+++ Leetcode/Array/189.RotateArray/solution.h
@@ -8,4 +8,18 @@ public:
             nums.insert(nums.begin(),*it);
         }
     }
+    void rotate2(vector<int>& nums, int k) {
+        vector<int> copy = nums;
+        int n = nums.size();
+        for (int i = 0; i < n; i ++)
+        {
+            nums[(i + k)%n] = copy[i];
+        }
+    }
+   void rotate3(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums.end() - k, nums.end());
+        reverse(nums.begin(), nums.end() - k);
+	    reverse(nums.begin(), nums.end());
+    } 
 };
