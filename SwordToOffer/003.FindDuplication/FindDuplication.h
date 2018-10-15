df5fea667dd05acdd5bcfb257881c941ec99a06f
Meteora <czhaor@gmail.com>
2018-10-15 21:21:18 +0800

    check input arugment

--- SwordToOffer/003.FindDuplication/FindDuplication.h
+++ SwordToOffer/003.FindDuplication/FindDuplication.h
@@ -11,8 +11,27 @@ public:
     bool duplicate(int array[], int n, int* duplication);
 };
 
+//////////////////////////////////////////////////////////////
+// array:       input array
+// n:           array length
+// duplication: duplicated number
+// output:      
+//    true:     find duplication in array
+//    false:    no duplication
+//////////////////////////////////////////////////////////////
 bool Solution::duplicate(int array[], int n, int* duplication)
 {
+    if(array == nullptr || n == 0)
+    {
+        return false;
+    }
+    for(int j = 0; j < n; j++)
+    {
+        if(array[j] > n-1 || array[j] < 0)
+        {
+            return false;
+        }
+    }
     printf("adress of input array in function call: %d\n", array);
     for(int i = 0; i < n; i++)
     {
