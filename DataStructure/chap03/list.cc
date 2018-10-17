da19aac3bfc1a56c8f49bbbc9705b223c7b64e96
Meteora <czhaor@gmail.com>
2018-10-17 23:21:52 +0800

    More implementation for list API

---
+++ DataStructure/chap03/list.cc
@@ -0,0 +1,94 @@
+#include "list.h"
+//---------------------------------------------------
+// Constructors
+//---------------------------------------------------
+template <typename T> void List<T>::init()
+{
+    header = new ListNode<T>;
+    trailer = new ListNode<T>;
+    header -> succ = trailer; header -> pred = NULL;
+    trailer -> pred = header; trailer -> succ = NULL;
+    _size = 0;
+}
+
+template <typename T> List<T>::List(const List<T>& L)
+{
+    copyNodes(L.first(),L._size);
+}
+
+template <typename T> List<T>::List(const List<T>& L, Rank r, int n)
+{
+    /* 原版这里应该有错
+       L[r]返回的是对数据T的引用 T&
+       而copyNodes接收的第一个参数为ListNode<T>*
+    */
+    //copyNodes(L[r],n);
+}
+
+template <typename T> List<T>::List(Posi(T) p, int n)
+{
+    copyNodes(p,n);
+}
+
+template <typename T> List<T>::~List()
+{
+    clear();
+    delete header;
+    delete trailer;
+}
+
+template <typename T> Posi(T) List<T>::first() const
+{
+    return header -> succ;
+}
+
+template <typename T> Posi(T) List<T>::last() const
+{
+    return trailer -> pred;
+}
+
+template <typename T> Posi(T) List<T>::find(const T& e) const
+{
+    return find(e, _size, trailer);
+}
+
+template <typename T> Posi(T) List<T>::find(const T& e, int n, Posi(T) p) const
+    while(0 < n--)
+    {
+        if(e == (p = p -> pred) -> data)
+        {
+            return p;
+        }
+    }
+    return NULL;
+}
+
+template <typename T> void List<T>::copyNodes(Posi(T) p, int n)
+{
+    init();
+    while(n--)
+    {
+        insertAsLast(p -> data);
+        p = p -> succ;
+    }
+}
+
+template <typename T> int List<T>::clear()
+{
+    int oldSize = _size;
+    while(_size > 0)
+    {
+        remove(header -> succ);
+    }
+    return oldSize;
+}
+
+
+template <typename T> T List<T>::remove(Posi(T) p)
+{
+    T e = p -> data;
+    p -> pred -> succ = p -> succ;
+    p -> succ -> pred = p -> pred;
+    delete p; _size --;
+    return e;
+}
--- DataStructure/chap03/list.h
+++ DataStructure/chap03/list.h
@@ -7,7 +7,9 @@ private:
 	Posi(T) trailer;
 protected:
 	void init();									//创建时的初始化
-
+    int clear();                                    //清除所有节点
+    void copyNodes(Posi(T) p, int n);               //复制列表中自p起的n项
+    
 public:
 	//构造函数
 	List(){ init();}								//默认构造函数
@@ -22,7 +24,13 @@ public:
 	bool empty() const {return _size <=0;}
 	Posi(T) first() const;
 	Posi(T) last() const;
-	Rank find(const T& e) const;
-
+    Posi(T) find(const T& e) const;                    //无序列表查找
+	Posi(T) find(const T& e, int n, Posi(T) p) const;  //无序区间查找
+    T& operator[](Rank r) const;
 	//可写访问接口
+    Posi(T) InsertAsFirst(const T& e);  //作为首节点插入
+    Posi(T) InsertAsLast(const T& e);   //作为末节点插入
+    Posi(T) InsertBefore(Posi(T) p, const T& e);    //将e作为p的前驱插入
+    Posi(T) InsertAfter(Posi(T) p, const T& e);     //将e作为p的后继插入
+    T remove(Posi(T) p);                //删除合法位置p处的节点
 }
