eef627aaa6b231cf2b9209e7d32e3bb553012bf8
Meteora <czhaor@gmail.com>
2018-11-12 23:50:19 +0800

    still not work:(

--- Leetcode/Array/498.DiagonalTraverse/solution.h
+++ Leetcode/Array/498.DiagonalTraverse/solution.h
@@ -5,26 +5,43 @@ public:
         if(matrix.empty() || matrix[0].empty()) return res;
         int rowsize = matrix.size();
         int colsize = matrix[0].size();
-        bool revers = true;
-        for(int i = 0, j = 0; i < rowsize || j != colsize - 1; )
+        bool reverse = true;
+        int index = 0;
+        for(int i = 0, j = 0; index < colsize + rowsize - 1; )
         {
-            int curcol = j;
-            int currow = i;
-            while(i < rowsize && j >= 0)
+            
+            
+            if(!reverse)
             {
-                res.push_back(matrix[i++][j--]);
-            }
-            if(curcol < colsize - 1 ) 
-            {
-                j = ++curcol;
-                i = 0;
+                while(i < rowsize && j >= 0)
+                {
+                    res.push_back(matrix[i++][j--]);
+                }
+                reverse = !reverse;
             }
             else
             {
-                j = curcol;
-                i = ++currow;
+                while(i >= 0  && j < colsize)
+                {
+                    res.push_back(matrix[i--][j++]);
+                }
+                reverse = !reverse;
             }
             
+            index++;
+
+            if(index <= max(colsize,rowsize) - 1 ) 
+            {
+                reverse? j = max(0, index - rowsize + 1) : j = index;
+                reverse? i = index : i = max(0, index - colsize + 1);
+            }
+            else
+            {
+                reverse? j = max(index - colsize + 1, 2*index - rowsize - colsize + 1) : j = colsize - 1;
+                reverse? i = rowsize - 1 : i = max(index - rowsize + 1, 2*index - rowsize - colsize + 1);
+            }
+            i = min(i, rowsize - 1);
+            j = min(j, colsize - 1);
 
         }
         return res;
