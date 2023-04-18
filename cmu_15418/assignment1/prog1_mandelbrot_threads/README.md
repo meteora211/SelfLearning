de7e4843241b05b3b42920721ed94d206c58acba
zhaorui chen <czhaor@gmail.com>
2023-04-19 00:31:52 +0800

    assign1.

---
+++ cmu_15418/assignment1/prog1_mandelbrot_threads/README.md
@@ -0,0 +1,7 @@
+## thread speed up
+
+- 2 threads: 1.99x
+- 4 threads: 2.46x
+- 8 threads: 3.98x
+- 16 threads: 7.25x
+- 32 threads: 12.81x
--- cmu_15418/assignment1/prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ cmu_15418/assignment1/prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -32,8 +32,27 @@ void* workerThreadStart(void* threadArgs) {
     WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 
     // TODO: Implement worker thread here.
+    int threadId = args->threadId;
+    int numThreads = args->numThreads;
+    unsigned int width = args->width;
+    unsigned int height = args->height;
+    int rowsPerTask = height / numThreads;
+    int startRow = threadId * rowsPerTask;
+    int numRows = (startRow + rowsPerTask > height) ?
+                  height - startRow : rowsPerTask;
+    /* int* output = args->output + startRow * width; */
+    mandelbrotSerial(args->x0,
+                     args->y0,
+                     args->x1,
+                     args->y1,
+                     width,
+                     height,
+                     startRow,
+                     numRows,
+                     args->maxIterations,
+                     args->output);
 
-    printf("Hello world from thread %d\n", args->threadId);
+    printf("Hello world from thread %d width %d  height %d  \n", args->threadId, width, height);
 
     return NULL;
 }
@@ -62,7 +81,16 @@ void mandelbrotThread(
 
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
+        args[i].x0 = x0;
+        args[i].x1 = x1;
+        args[i].y0 = y0;
+        args[i].y1 = y1;
+        args[i].width = width;
+        args[i].height = height;
+        args[i].maxIterations = maxIterations;
+        args[i].output = output;
         args[i].threadId = i;
+        args[i].numThreads = numThreads;
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
